951fcb912ed2a13f41d8dd2cf9ce6838de4e8048
Fork-Network <bekbol17281923@outlook.com>
2021-07-22 15:48:54 -0400

    ports

--- taco/cmds/configure.py
+++ taco/cmds/configure.py
@@ -110,7 +110,7 @@ def configure(
 
         elif testnet == "false" or testnet == "f":
             print("Setting Mainnet")
-            mainnet_port = "18620"
+            mainnet_port = "3675"
             mainnet_introducer = "dns-introducer.forknetwork.net"
             net = "mainnet"
             config["full_node"]["port"] = int(mainnet_port)
